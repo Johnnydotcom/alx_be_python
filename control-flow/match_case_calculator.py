56f4c1e95589754a2b085907474268075d92386d
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-07-02 19:42:28 +0200

    conditional case statement

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -13,7 +13,10 @@ match sign:
         print("The result is", num1 * num2)
 
     case "/":
-        print("The result is", num1 / num2)
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else :
+            print("The result is", num1 / num2)
 
     case _:
         print("Invalid operation")
