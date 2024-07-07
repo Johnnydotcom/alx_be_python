c379701469076e3145ada28dd869748196a2b88b
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-07-07 23:13:08 +0200

    function that perfroms arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,12 @@
 def perform_operation(num1, num2, operation):
-    if operation == "*":
-        print({num1} * {num2})
-    elif operation == "+":
-        print({num1} + {num2})
-    elif operation == "-":
-        print({num1} - {num2})
-    elif operation == "/":
+    if operation == "multiply":
+        print({num1 * num2})
+    elif operation == "add":
+        print({num1 + num2})
+    elif operation == "subtract":
+        print({num1 - num2})
+    elif operation == "divide":
         if num2 == 0:
             print("Not divisble by zero")
-    else: 
-        print({num1} / {num2})
+    else:
+        print({num1 / num2})
