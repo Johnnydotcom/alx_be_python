3f9c99eb7390263a8a101dcd21366997ad8c7743
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-07-08 00:28:55 +0200

    conversion fix

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -7,7 +7,7 @@ sign = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").islower
 def convert_to_celsius(fahrenheit):
     cel = ((temp - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR)
 def convert_to_fahrenheit(celsius):
-    fah = ((temp + 32) * CELSIUS_TO_FAHRENHEIT_FACTOR)
+    fah = (CELSIUS_TO_FAHRENHEIT_FACTOR + 32 -temp)
 
     if sign == "c":
          print (f"{cel} is = {fah}")
