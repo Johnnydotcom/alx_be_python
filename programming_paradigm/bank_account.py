9cd8ad0964f613b35e35938be9416c7f21ce5ad9
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-07-18 21:39:20 +0200

    bank_account balance fix.

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,17 +1,18 @@
 class BankAccount:
+    
     def __init__(self, account_balance: float):
-        self.current_balance = account_balance
+        self.account_balance = 0
         
     def deposit(self, amount):
-        return self.current_balance + amount
+        return self.account_balance + amount
     
 
     def withdraw(self, amount):
-        if amount > self.current_balance:
+        if amount > self.account_balance:
             return False
         else:
-            self.current_balance - amount
+            self.account_balance - amount
             return True
                     
     def display_balance(self, amount):
-        return f"Current Balance: {self.current_balance(amount)}"
+        return f"Current Balance: {self.account_balance(amount)}"
