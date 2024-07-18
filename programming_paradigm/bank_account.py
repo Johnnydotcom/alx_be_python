89ecde87a94359837ddaf9330a8d494ac3be1dbe
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-07-18 21:56:55 +0200

    bank_account print format.

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -9,9 +9,10 @@ class BankAccount:
 
     def withdraw(self, amount):
         if amount > self.account_balance:
-            return True
-        elif amount < self.account_balance:
             return False
+        elif amount <= self.account_balance:
+            self.account_balance - amount
+            return True
                     
     def display_balance(self, amount):
         return f"Current Balance: ${self.account_balance(amount)}"
