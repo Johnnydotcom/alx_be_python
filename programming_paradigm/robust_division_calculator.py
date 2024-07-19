c489ce3c37c32622f0af611352b7ac9b82179615
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-07-19 19:43:24 +0200

    Exception handling

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,16 @@
+def safe_divide(numerator, denominator):
+    try:
+        if denominator == 0:
+            raise ZeroDivisionError(denominator)
+        else:
+            try:
+                numerator = float(numerator)
+                denominator = float(denominator)
+                return f"The result of the division is {numerator / denominator}"
+                #raise ValueError("Enter a numeric value.")
+            except ValueError:
+                print("Error: Please enter numeric values only.")
+    except ZeroDivisionError:
+        print("Error: Cannot divide by zero.")
+
+         
