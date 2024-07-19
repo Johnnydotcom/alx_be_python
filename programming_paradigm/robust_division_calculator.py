e4a03dba8037b4dd83a637236917e9aded62a0ab
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-07-19 19:54:27 +0200

    Exception handling fix

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,14 +1,14 @@
-def safe_divide(x, y):
+def safe_divide(numerator, denominator):
     try:
-        if y == 0:
-            raise ZeroDivisionError(y)
+        if denominator == 0:
+            raise ZeroDivisionError(denominator)
         else:
             try:
-                numerator = float(x)
-                denominator = float(y)
+                numerator = float(numerator)
+                denominator = float(denominator)
                 return f"The result of the division is {numerator / denominator}"
                 #raise ValueError("Enter a numeric value.")
             except ValueError:
                 print("Error: Please enter numeric values only.")
     except ZeroDivisionError:
-        print("Error: Cannot divide by zero.")   
+        print("Error: Cannot divide by zero.") 
