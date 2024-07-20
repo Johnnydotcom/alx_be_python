e7ca140593069e1bbc41c143fd7f60493f5e9a2d
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-07-20 06:57:31 +0200

    learning unittest.

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -9,17 +9,17 @@ class  TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(10, 6), 16)
         
-    def test_subtract(self):
+    def test_subtraction(self):
         self.calc = SimpleCalculator()
         self.assertEqual(self.calc.subtract(2, 3), -1)
         #self.assertEqual(self.calc.subtract(5, 7), 2)
         
-    def test_multiply(self):
+    def test_multiplication(self):
         self.calc = SimpleCalculator()
         self.assertEqual(self.calc.multiply(5, 3), 15)
         self.assertEqual(self.calc.multiply(5, 5), 25)
         
-    def test_divide(self):
+    def test_division(self):
         self.calc = SimpleCalculator()
         self.assertEqual(self.calc.divide(10, 1), 10)
         self.assertEqual(self.calc.divide(18, 2), 9)
