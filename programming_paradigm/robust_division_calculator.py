4a69cc5c076ef7bdb17743ae3206ca954bfab28f
qayiya tshotsho <mr.qtshotsho@gmail.com>
2024-07-20 07:56:34 +0200

    Error handling for a simple calculator.

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,7 +3,7 @@ def safe_divide(numerator, denominator):
                 numerator = float(numerator)
                 denominator = float(denominator)
                 if denominator == 0:
-                    raise ZeroDivisionError("Cannot dicide by zero.")
+                    raise ZeroDivisionError("Error: Cannot divide by zero.")
                 return f"The result of the division is {numerator / denominator}"
     except ValueError:
         return "Error: Please enter numeric values only."
